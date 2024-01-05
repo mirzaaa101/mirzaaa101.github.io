423144fa5a60484c84da47c826a2832c99a13012
Mirza Abbas <mirzaabbasuddin2@gmail.com>
2024-01-06 02:48:53 +0600

    update-6

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPhoneNumber() {
+    let phone = document.getElementById("phone");
+    phone.innerHTML = "+88 016 407 20014"
+    phone.style.backgroundColor = "white";
+    phone.style.color = "black";
+  }
+  document.getElementById("currentYear").innerText = "\u00A9 Mirza Abbas Uddin 2023-" + new Date().getFullYear();
