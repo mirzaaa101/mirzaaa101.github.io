1264542fbbf673114c939e07eda1770f08a8ca7b
Mirza Abbas <mirzaabbasuddin2@gmail.com>
2024-01-06 13:45:06 +0600

    update-8

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+  const text = "What drives me? My skills and interests, right here!";
+  const textElement = document.getElementById('typed-text');
+  let textIndex = 0;
+  let typingInterval = 50;
+  let pauseInterval = 5000;
+
+  function typeText() {
+    if (textIndex < text.length) {
+      textElement.textContent += text.charAt(textIndex);
+      textIndex++;
+      setTimeout(typeText, typingInterval);
+    } else {
+      setTimeout(startOver, pauseInterval);
+    }
+  }
+
+  function startOver() {
+    textElement.textContent = '';
+    textIndex = 0;
+    setTimeout(typeText, 0);
+  }
+
+  typeText();
+
 function showPhoneNumber() {
     let phone = document.getElementById("phone");
     phone.innerHTML = "+88 016 407 20014"
